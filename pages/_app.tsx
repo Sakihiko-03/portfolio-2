5c95fb4646f5bed723a4f121ab2e48e4c3948c46
Sakihiko-03 <lek17622@gmail.com>
2022-12-23 20:41:00 +0700

    ref form portfolio-1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    // <ThemeProvider defaultTheme='dark'>
+    <ThemeProvider enableSystem={true} attribute='class'>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+  // <Component {...pageProps} />
 }
+
+export default MyApp
