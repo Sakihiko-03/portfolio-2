b434e1163973ecb5b673da9a2a0f906f397fa084
Sakihiko-03 <lek17622@gmail.com>
2022-12-25 16:30:21 +0700

    edit Test1

--- components/Test1.tsx
+++ components/Test1.tsx
@@ -7,16 +7,27 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 export default function App() {
-  const renderSlides = () =>
-    [1, 2, 3, 4, 5, 6, 7, 8].map(num => (
-      <div className="bg-gray-800 m-8 p-8 rounded-lg">
-        <h3>Slide {num}</h3>
-      </div>
-    ));
+    const renderSlides = () =>
+        [1, 2, 3, 4, 5, 6, 7, 8].map(num => (
+            <div className="bg-gray-800 opacity-70 m-8 p-8 rounded-lg">
+                <h3>Slide {num}</h3>
+            </div>
+        ));
 
-  return (
-    <div className="App">
-      <Slider dots={true}>{renderSlides()}</Slider>
-    </div>
-  );
+    return (
+        <div className=''>
+            <Slider
+                //   dots={true}
+                dots={false}
+                infinite={true}
+                slidesToShow={2}
+                slidesToScroll={2}
+                
+                // autoplay={true}
+                // autoplaySpeed={3000}
+                // centerMode={true}
+            >
+                {renderSlides()}</Slider>
+        </div>
+    );
 }
