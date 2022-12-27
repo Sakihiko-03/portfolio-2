eff6e8ec93c4ed4b88fefc4242b92c190fdbad5c
Sakihiko-03 <lek17622@gmail.com>
2022-12-27 13:21:49 +0700

    edit modal

--- components/ModalBox.tsx
+++ components/ModalBox.tsx
@@ -1,71 +1,59 @@
 import React from "react";
 
 export default function Modal() {
-  const [showModal, setShowModal] = React.useState(false);
-  return (
-    <>
-      <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-        Open regular modal
-      </button>
-      {showModal ? (
+    const [showModal, setShowModal] = React.useState(false);
+    return (
         <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Modal Title
-                  </h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            <button
+                className="text-amber-400 inline-flex items-center md:mb-2 lg:mb-0"
+                // className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => setShowModal(true)}
+            >
+                Learn More
+                <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                </svg>
+            </button>
+            {showModal ? (
+                <>
+                    <div className="absolute inset-0 z-30">
+                        <div className="flex items-center justify-center min-h-screen w-full h-full fixed top-0 left-0 z-[60] overflow-x-hidden overflow-y-auto">
+                            {/*content*/}
+                            <div className="relative z-10 max-w-3xl m-4 rounded-lg shadow-lg flex flex-col bg-white outline-none focus:outline-none overflow-hidden">
+                                {/*header*/}
+                                <div className="relative h-8 flex items-center max-w-[50rem] bg-gray-800 py-2 px-24 dark:bg-gray-700">
+                                    <div className="flex space-x-1 absolute top-2/4 left-4 -translate-y-1">
+                                        <span className="w-2 h-2 rounded-full bg-red-600"></span>
+                                        <span className="w-2 h-2 rounded-full bg-amber-500"></span>
+                                        <span className="w-2 h-2 rounded-full bg-green-500"></span>
+                                    </div>
+                                    <div className="">
+                                        <button type="button" onClick={() => setShowModal(false)} className="absolute top-0 right-0 translate-y-1 -translate-x-1 bg-transparent hover:bg-red-700 rounded-md text-center">
+                                            <svg className="h-6 w-6 text-red-600 hover:text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className="flex items-start justify-between p-2 mx-4 border-b border-solid border-slate-200 rounded-t">
+                                    <h3 className="text-xl font-semibold text-black">Modal Title</h3>
+                                </div>
+                                {/*body*/}
+                                <div className="relative px-6 flex-auto">
+                                    <p className="my-4 text-slate-500 text-sm leading-relaxed">
+                                        I always felt like I could do anything. That’s the main
+                                        thing people are controlled by! Thoughts- their perception
+                                        of themselves! They're slowed down by their perception of
+                                        themselves. If you're taught you can’t do anything, you
+                                        won’t do anything. I was taught I could do everything.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-50 fixed inset-0 z-0 bg-gray-900 backdrop-blur-md"></div>
+                </>
+            ) : null}
         </>
-      ) : null}
-    </>
-  );
+    );
 }
--- components/Work.tsx
+++ components/Work.tsx
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import ModalBox from "./ModalBox";
 export default class SimpleSlider extends Component {
+    
     render() {
         const settings = {
             dots: true,
@@ -39,31 +41,15 @@ export default class SimpleSlider extends Component {
         return (
             <div id='projects'>
                 <Slider {...settings}>
-                    <div className="p-4 md:w-1/3">
-                        <div className="h-full dark:bg-white dark:bg-opacity-5 dark:backdrop-blur-md bg-gray-400 bg-opacity-5 backdrop-blur-md rounded-lg overflow-hidden">
-                            <img src='https://scontent.fbkk12-1.fna.fbcdn.net/v/t39.30808-6/321459732_526396949453716_4200402998008371909_n.jpg?stp=dst-jpg_s960x960&_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHYIZjagMHg2YGnViyNDrgmSBBGvJQ5lsxIEEa8lDmWzBjlhzvaUyAmfPTFDdB4mHs6g1vHPII3Tm60ZpoF_69r&_nc_ohc=m4oHZnKYP3wAX-tu76H&_nc_zt=23&_nc_ht=scontent.fbkk12-1.fna&oh=00_AfCGxcNKu1DicCZCEFhjLe5jUpC0FlcyifS2v6Oxi7Ni8w&oe=63ADA981'></img>
+                    <div className="relative p-4 md:w-1/3">
+                        <div className="h-full dark:bg-white dark:bg-opacity-5 dark:backdrop-blur-md bg-gray-400 bg-opacity-5 backdrop-blur-md rounded-lg">
+                            <img className="rounded-t-lg overflow-hidden" src='https://scontent.fbkk12-1.fna.fbcdn.net/v/t39.30808-6/321459732_526396949453716_4200402998008371909_n.jpg?stp=dst-jpg_s960x960&_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHYIZjagMHg2YGnViyNDrgmSBBGvJQ5lsxIEEa8lDmWzBjlhzvaUyAmfPTFDdB4mHs6g1vHPII3Tm60ZpoF_69r&_nc_ohc=m4oHZnKYP3wAX-tu76H&_nc_zt=23&_nc_ht=scontent.fbkk12-1.fna&oh=00_AfCGxcNKu1DicCZCEFhjLe5jUpC0FlcyifS2v6Oxi7Ni8w&oe=63ADA981'></img>
                             <div className="p-6">
                                 <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">INTERNSHIP</h2>
                                 <h1 className="title-font text-lg font-medium text-white mb-3">Kapacitor [Web App]</h1>
                                 <p className="leading-relaxed mb-3 text-sm">Kapacitor is a peer-to-peer (P2P) energy exchange platform built on Polygon Blockchain. Electric power producers can supply electricity to the utility grid system to earn a utility token as KAP.</p>
                                 <div className="flex items-center flex-wrap ">
-                                    <a className="text-amber-400 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                                        <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path d="M5 12h14"></path>
-                                            <path d="M12 5l7 7-7 7"></path>
-                                        </svg>
-                                    </a>
-                                    <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-800">
-                                        <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                            <circle cx="12" cy="12" r="3"></circle>
-                                        </svg>1.2K
-                                    </span>
-                                    <span className="text-gray-500 inline-flex items-center leading-none text-sm">
-                                        <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                            <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                                        </svg>6
-                                    </span>
+                                    <ModalBox/>
                                 </div>
                             </div>
                         </div>
