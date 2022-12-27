f8f280ed9a9da09de32273d7ebfa7c18613c9368
Sakihiko-03 <lek17622@gmail.com>
2022-12-27 11:37:50 +0700

    install Flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
@@ -10,5 +11,6 @@ module.exports = {
   },
   plugins: [
     require('tailwind-scrollbar'),
+    require("flowbite/plugin")
   ],
 }
