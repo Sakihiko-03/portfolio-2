78107707022ca5cfa281dd5723e79bf167352950
Sakihiko <103117406+Sakihiko-03@users.noreply.github.com>
2023-04-13 21:12:40 +0700

    edit about me

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -7,7 +7,6 @@ type Props = {}
 export default function About({ }: Props) {
   return (
     <div id='about' className='md:grid grid-cols-3 h-3/4'>
-
       <div className='flex justify-center items-center pt-24'>
         <motion.div
           initial={{ opacity: 0, scale: 0.6 }}
@@ -40,17 +39,11 @@ export default function About({ }: Props) {
           transition={{ duration: 0.8 }}
         >
           <div className='md:px-8 lg:px-16'>
-            <p className='dark:text-white text-black font-medium text-xl mb-3'>PROFILE</p>
-            <p>I am studying in the 4th year of Electronic Engineering and Computer Systems.
-              Even though I didn't study programming directly.
-              But I am interested in Front-end develop and User Interface design.
-              After getting an internship with the Provincial Electricity Authority and doing a project together,
-              it gave me more work experience and ways to gain more knowledge.</p>
+            <p className='dark:text-white text-black font-medium text-xl mb-3'>About Me</p>
+            <p>As an Electronic Engineering and Computer Systems graduate, I have a strong interest in Front-end development and UI design. My internship experience with the PEA provided me with valuable experience in these areas and reinforced my passion for creating visually appealing and user-friendly interfaces. I am excited to continue learning and growing in these fields to contribute to innovative projects.</p>
           </div>
         </motion.div>
       </div>
-
     </div>
-
   )
 }
