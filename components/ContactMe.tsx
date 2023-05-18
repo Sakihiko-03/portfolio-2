23eeaeebfcd676cdb56dc8a682cf34e563789016
Sakihiko <103117406+Sakihiko-03@users.noreply.github.com>
2023-05-18 11:31:34 +0700

    edit skill and contact

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -3,71 +3,58 @@ import { EnvelopeIcon, MapPinIcon, PhoneIcon } from '@heroicons/react/24/solid'
 import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
-    name: string,
-    email: string,
-    subject: string,
-    message: string,
+  name: string,
+  email: string,
+  subject: string,
+  message: string,
 
 };
 type Props = {}
 
 function ContactMe({ }: Props) {
-    const { register, handleSubmit } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = formData => {
-        //  console.log(formData)   
-        window.location.href = `mailto:lek17622@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`;
-    };
-    return (
-        <div className='md:grid md:grid-cols-2 h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-center mx-auto items-center'>
-            {/* <div className='space-y-4 mb-8'>
-               <h3 className='flex items-center space-x-5 justify-center uppercase text-gray-500 text-2xl'>Contact</h3>
-                  <div className='flex items-center space-x-5 justify-center'>
-                    <PhoneIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
-                    <p className='text-lg md:text-base'>0813988083</p>
-                </div>
-                <div className='flex items-center space-x-5 justify-center'>
-                    <EnvelopeIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
-                    <p className='text-lg md:text-base'>lek17622@gmail.com</p>
-                </div>
-                <div className='flex items-center space-x-5 justify-center'>
-                    <MapPinIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
-                    <p className='text-lg md:text-base'>Chachosaoeng, Thailand</p>
-                </div>
-            </div> */}
-              <div className='my-8'>
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = formData => {
+    //  console.log(formData)   
+    window.location.href = `mailto:lek17622@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`;
+  };
+  return (
+    <div id='projects'>
+      <div className='md:grid md:grid-cols-2 h-screen flex relative flex-col text-center md:text-left md:flex-row justify-center items-center'>
+        <div className='my-8'>
           <p className='dark:text-white text-black font-medium text-xl mb-4'>CONTACT</p>
           <div className='flex mb-4'>
             <div className='mr-4'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
-              </svg>
+              <PhoneIcon className='text-amber-400 h-7 w-7' />
             </div>
             <p className='text-slate-400'>081-3988083</p>
           </div>
-          <div className='flex'>
+          <div className='flex mb-4'>
             <div className='mr-4'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-              </svg>
+              <EnvelopeIcon className='text-amber-400 h-7 w-7' />
             </div>
             <p className='text-slate-400'>lek17622@gmail.com</p>
           </div>
-        </div>
-            <div className='flex flex-col space-y-10'>     
-               
-
-                <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-fit mx-auto'>
-                    <div className='flex space-x-2'>
-                        <input {...register('name')} placeholder='Name' className='contactInput' type="text" />
-                        <input {...register('email')} placeholder='Email' className='contactInput' type="text" />
-                    </div>
-                    <input {...register('subject')} placeholder='Subject' className='contactInput' type="text" />
-                    <textarea {...register('message')} placeholder='Message' className='contactInput' />
-                    <button type='submit' className='bg-[#F7AB0A] py-4 px-8 rounded-md text-black font-bold text-lg'>Submit</button>
-                </form>
+          <div className='flex'>
+            <div className='mr-4'>
+              <MapPinIcon className='text-amber-400 h-7 w-7' />
             </div>
+            <p className='text-slate-400'>Chachosaoeng, Thailand</p>
+          </div>
         </div>
-    )
+        <div className='flex flex-col space-y-8'>
+          <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2'>
+            <div className='grid grid-cols-2 space-x-2'>
+              <input {...register('name')} placeholder='Name' className='contactInput rounded-lg border-none' type="text" />
+              <input {...register('email')} placeholder='Email' className='contactInput rounded-lg border-none' type="text" />
+            </div>
+            <input {...register('subject')} placeholder='Subject' className='contactInput rounded-lg border-none' type="text" />
+            <textarea {...register('message')} placeholder='Message' className='contactInput rounded-lg border-none overflow-hidden' />
+            <button type='submit' className='bg-amber-400 py-4 px-8 rounded-md text-black font-bold text-lg'>Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ContactMe
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import ModalBox from './ModalBox'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
@@ -12,10 +11,15 @@ export default function Skill({ }: Props) {
     return (
         <div className='relative z-10 my-32'>
             <p className='dark:text-white text-black font-medium text-xl mb-4'>SKILLS</p>
-            <div className='grid grid-cols-2 lg:grid-cols-5 gap-8 text-slate-400'>
-                <div className='border dark:border-slate-700 border-slate-300 text-slate-400 p-4 rounded-lg'>
-                    <p className='dark:text-white text-black font-semibold'>Programming Language</p>
-                    <div className='grid grid-cols-2'>
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-8 text-slate-400'>
+
+                <motion.div
+                    initial={{ opacity: 0, y: 45 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: .5 }}>
+                    <div className='space-y-2 border dark:border-slate-700 border-slate-300 text-slate-400 p-4 rounded-lg'>
+                        <p className='dark:text-white text-black font-semibold mb-4'>Programming Language</p>
+                        {/* <div className='grid grid-cols-2'>
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" width={35} />
                         <p className=''>C</p></div>
                     <div className='grid grid-cols-2'>
@@ -24,107 +28,107 @@ export default function Skill({ }: Props) {
                     <div className='grid grid-cols-2'>
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" width={35} />
                         <p className=''>Python</p>
+                    </div> */}
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg" width={35} />
+                            <p className=''>Dart</p>
+                        </div>
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" width={35} />
+                            <p className=''>HTML</p>
+                        </div>
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" width={35} />
+                            <p className=''>CSS</p>
+                        </div>
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" width={35} />
+                            <p className=''>JavaScript</p>
+                        </div>
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" width={35} />
+                            <p className=''>TypeScript</p>
+                        </div>
                     </div>
-                    <div className='grid grid-cols-2'>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" width={35} />
-                        <p className=''>JavaScript</p>
-                    </div>
-                    <div className='grid grid-cols-2'>
+                </motion.div>
 
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" width={35} />
-                        <p className=''>TypeScript</p>
-                    </div>
-                    <div className='grid grid-cols-2'>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg" width={35} />
-                        <p className=''>Dart</p>
-                    </div>
-                    <div className='grid grid-cols-2'>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" width={35} />
-                        <p className=''>HTML</p>
-                    </div>
-                    <div className='grid grid-cols-2'>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" width={35} />
-                        <p className=''>CSS</p>
-                    </div>
-                </div>
-                <div className='border dark:border-slate-700 border-slate-300 text-slate-400 p-4 rounded-lg'>
-                    <p className='dark:text-white text-black font-semibold'>Framework & Libraries</p>
-                    <div className='grid grid-cols-2'>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg" width={35} />
-                    <p className=''>Flutter</p>
-                    </div>
-
-                    {/* <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ionic/ionic-original.svg" width={35}/>
+                <motion.div
+                    initial={{ opacity: 0, y: 40 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: .6 }}>
+                    <div className='space-y-2 border h-full dark:border-slate-700 border-slate-300 text-slate-400 p-4 rounded-lg'>
+                        <p className='dark:text-white text-black font-semibold mb-4'>Framework & Libraries</p>
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg" width={35} />
+                            <p className=''>Flutter</p>
+                        </div>
+                        {/* <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ionic/ionic-original.svg" width={35}/>
                     <p className=''>Ionic-3</p>
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-plain.svg" width={35}/>
                     <p className=''>Angular-4</p> */}
-                    <div className='grid grid-cols-2'>
-                    
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" width={35} />
-                    <p className=''>ReactJS</p>
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" width={35} />
+                            <p className=''>NextJS</p>
+                        </div>
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" width={35} />
+                            <p className=''>Tailwind CSS</p>
+                        </div>
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" width={35} />
+                            <p className=''>ReactJS</p>
+                        </div>
                     </div>
-                    <div className='grid grid-cols-2'>
-                   
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" width={35} />
-                    <p className=''>NextJS</p>
-                    </div>
-                    <div className='grid grid-cols-2'>
-                   
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" width={35} />
-                    <p className=''>Tailwind CSS</p>
-                    </div>
+                </motion.div>
 
-                </div>
-                {/* <div className='border dark:border-slate-700 border-slate-300 text-slate-400 p-4 rounded-lg'>
-                    <p className='dark:text-white text-black font-semibold'>Database</p>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" width={35}/>
-                    <p className=''>MongoDB</p>
-                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" width={35}/>
-                    <p className=''>NodeJs-3</p>
-                </div> */}
-                <div className='border dark:border-slate-700 border-slate-300 text-slate-400 p-4 rounded-lg'>
-                    <p className='dark:text-white text-black font-semibold'>Dev Tool</p>
-                    <div className='grid grid-cols-2'>
-                    
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" width={35} />
-                    <p className=''>VS Code</p>
+                <motion.div
+                    initial={{ opacity: 0, y: 24 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: .6 }}>
+                    <div className='space-y-2 border h-full dark:border-slate-700 border-slate-300 text-slate-400 p-4 rounded-lg'>
+                        <p className='dark:text-white text-black font-semibold mb-4'>Dev Tool</p>
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" width={35} />
+                            <p className=''>VS Code</p>
+                        </div>
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg" width={35} />
+                            <p className=''>Android Studio</p>
+                        </div>
+                        {/* <div className='flex items-center space-x-8'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/arduino/arduino-original.svg" width={35} />
+                        <p className=''>Arduino IDE</p>
+                        </div> */}
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" width={35} />
+                            <p className=''>Github</p>
+                        </div>
+                        {/* <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" width={35}/>
+                          <p className=''>git</p> */}
                     </div>
-                    <div className='grid grid-cols-2'>
+                </motion.div>
 
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" width={35} />
-                    <p className=''>Github</p>
+                <motion.div
+                    initial={{ opacity: 0, y: 28 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1 }}>
+                    <div className='space-y-2 border h-full dark:border-slate-700 border-slate-300 text-slate-400 p-4 rounded-lg'>
+                        <p className='dark:text-white text-black font-semibold mb-4'>Graphic</p>
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" width={35} />
+                            <p className=''>Figma</p>
+                        </div>
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/blender/blender-original.svg" width={35} />
+                            <p className=''>Blender</p>
+                        </div>
+                        <div className='flex items-center space-x-8'>
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/d/de/Procreate-icon.png" width={35} />
+                            <p className=''>Procreate</p>
+                        </div>
                     </div>
+                </motion.div>
 
-                    {/* <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" width={35}/>
-                    <p className=''>git</p> */}
-                    <div className='grid grid-cols-2'>
-                   
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg" width={35} />
-                    <p className=''>Android Studio</p>
-                    </div>
-                    <div className='grid grid-cols-2'>
-
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/arduino/arduino-original.svg" width={35} />
-                    <p className=''>Arduino IDE</p>
-                    </div>
-                    
-                </div>
-                <div className='border dark:border-slate-700 border-slate-300 text-slate-400 p-4 rounded-lg'>
-                    <p className='dark:text-white text-black font-semibold'>Graphic</p>
-                    <div className='grid grid-cols-2'>
-                    
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" width={35} />
-                    <p className=''>Figma</p>
-                    </div>
-                    <div className='grid grid-cols-2'>
-                    
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/blender/blender-original.svg" width={35} />
-                    <p className=''>Blender</p>
-                    </div>
-                    
-                    <p className=''>Procreate</p>
-                    {/* <ModalBox /> */}
-                </div>
             </div>
         </div>
     )
