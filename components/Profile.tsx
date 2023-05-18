b80857d77844a79fa42933294432676b22cc5dcb
Sakihiko <103117406+Sakihiko-03@users.noreply.github.com>
2023-05-18 13:23:21 +0700

    edit styles

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -49,19 +49,18 @@ export default function Hero({ }: Props) {
                         <span className=''>{text}</span>
                         <Cursor cursorColor='#F7AB0A' />
                     </h1>
-                    <div>
+                    <div className='space-x-6 lg:space-x-10'>
                         <Link href={'#about'}>
-                            <button className='heroButton'>About Me</button>
+                            <button className='px-4 py-2 border-[.8px] border-gray-500 rounded-full uppercase text-xs tracking-widest text-gray-500 transition-all hover:border-amber-400/70 hover:text-amber-400/70'>About Me</button>
                         </Link>
                         <Link href={'#projects'}>
-                            <button className='heroButton mx-8'>Projects</button>
+                            <button className='px-4 py-2 border-[.8px] border-gray-500 rounded-full uppercase text-xs tracking-widest text-gray-500 transition-all hover:border-amber-400/70 hover:text-amber-400/70'>Projects</button>
                         </Link>
-                        <button className='heroButton inline-flex items-center px-4 py-2' onClick={download}>
+                        <button className='px-4 py-2 border-[.8px] border-gray-500 rounded-full uppercase text-xs tracking-widest text-gray-500 transition-all hover:border-amber-400/70 hover:text-amber-400/70 inline-flex items-center' onClick={download}>
                             Resume
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-5 w-5 ml-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-4 w-4 ml-2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                             </svg>
-
                         </button>
                     </div>
                 </div>
